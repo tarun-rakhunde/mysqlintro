749116394a1516ea91fc7e24369fcf145bd62b58
unknown <tarunrakhunde@gmail.com>
2022-09-01 17:26:36 +0530

    done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var mysql = require('mysql');
 var express=require("express")
 const app=express()
+app.use(express.json())
 
 var con = mysql.createConnection({
   host: "localhost",
@@ -13,13 +14,21 @@ con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
 });
-app.get("/fetch",(req,res)=>{
+app.get("/",(req,res)=>{
     con.query("SELECT * FROM test.new_table",function(err,result,fields) {
         if(err){
             console.log(err)
         }else{
             return res.send(result)
         }
+    })
+})
+
+app.post("/",(req,res)=>{
+    const data=req.body;
+    con.query("INSERT INTO test.new_table SET?",data,(error,results,fields)=>{
+        if(error)throw error;
+        res.send(results)
     })
 })
 app.listen(1234,(err)=>{
